2b5707be49e0586e085754cce3f3b770e2be54ff
TIZEN77 <tizen.k18@gmail.com>
2021-03-14 17:41:04 +0100

    modification de test

--- CQFDsnap/CQFDsnap/Ocvtest.cpp
+++ CQFDsnap/CQFDsnap/Ocvtest.cpp
@@ -1,85 +1,58 @@
-#include "opencv2/objdetect.hpp"
-#include "opencv2/highgui.hpp"
-#include "opencv2/imgproc.hpp"
-#include "opencv2/videoio.hpp"
+#include <opencv2/opencv.hpp>
 #include <iostream>
+#include <iostream>
+#include <vector>
+
+
 using namespace std;
 using namespace cv;
-void detectAndDisplay(Mat frame);
-CascadeClassifier face_cascade;
-CascadeClassifier eyes_cascade;
+
 int main(int argc, const char** argv)
 {
-    CommandLineParser parser(argc, argv,
-        "{help h||}"
-        "{face_cascade|data/haarcascades/haarcascade_frontalface_alt.xml|Path to face cascade.}"
-        "{eyes_cascade|data/haarcascades/haarcascade_eye_tree_eyeglasses.xml|Path to eyes cascade.}"
-        "{camera|0|Camera device number.}");
-    parser.about("\nThis program demonstrates using the cv::CascadeClassifier class to detect objects (Face + eyes) in a video stream.\n"
-        "You can use Haar or LBP features.\n\n");
-    parser.printMessage();
-    String face_cascade_name = samples::findFile(parser.get<String>("face_cascade"));
-    String eyes_cascade_name = samples::findFile(parser.get<String>("eyes_cascade"));
-    //-- 1. Load the cascades
-    if (!face_cascade.load(face_cascade_name))
-    {
-        cout << "--(!)Error loading face cascade\n";
-        return -1;
-    };
-    if (!eyes_cascade.load(eyes_cascade_name))
-    {
-        cout << "--(!)Error loading eyes cascade\n";
-        return -1;
-    };
-    int camera_device = parser.get<int>("camera");
-    VideoCapture capture;
-    //-- 2. Read the video stream
-    capture.open(camera_device);
-    if (!capture.isOpened())
-    {
-        cout << "--(!)Error opening video capture\n";
-        return -1;
-    }
-    Mat frame;
-    while (capture.read(frame))
-    {
-        if (frame.empty())
-        {
-            cout << "--(!) No captured frame -- Break!\n";
-            break;
-        }
-        //-- 3. Apply the classifier to the frame
-        detectAndDisplay(frame);
-        if (waitKey(10) == 27)
-        {
-            break; // escape
-        }
-    }
-    return 0;
-}
-void detectAndDisplay(Mat frame)
-{
-    Mat frame_gray;
-    cvtColor(frame, frame_gray, COLOR_BGR2GRAY);
-    equalizeHist(frame_gray, frame_gray);
-    //-- Detect faces
-    std::vector<Rect> faces;
-    face_cascade.detectMultiScale(frame_gray, faces);
-    for (size_t i = 0; i < faces.size(); i++)
-    {
-        Point center(faces[i].x + faces[i].width / 2, faces[i].y + faces[i].height / 2);
-        ellipse(frame, center, Size(faces[i].width / 2, faces[i].height / 2), 0, 0, 360, Scalar(255, 0, 255), 4);
-        Mat faceROI = frame_gray(faces[i]);
-        //-- In each face, detect eyes
-        std::vector<Rect> eyes;
-        eyes_cascade.detectMultiScale(faceROI, eyes);
-        for (size_t j = 0; j < eyes.size(); j++)
-        {
-            Point eye_center(faces[i].x + eyes[j].x + eyes[j].width / 2, faces[i].y + eyes[j].y + eyes[j].height / 2);
-            int radius = cvRound((eyes[j].width + eyes[j].height) * 0.25);
-            circle(frame, eye_center, radius, Scalar(255, 0, 0), 4);
-        }
-    }
-    //-- Show what you got
-    imshow("Capture - Face detection", frame);
+
+	double scale = 3.0;
+
+	CascadeClassifier faceCascade;
+	faceCascade.load("D:\\opencv\\build\\etc\\haarcascades\\haarcascade_frontalface_alt.xml");
+
+	VideoCapture cap(0);
+	
+	if (!cap.isOpened())
+		return -1;
+	
+	for (;;)
+	{
+		string path = "Resources//snapchat.png";
+		Mat img = imread(path);
+		Mat frame;
+		cap >> frame;
+
+		Mat grayscale;
+		cvtColor(frame, grayscale, COLOR_BGR2GRAY);
+		resize(grayscale, grayscale, Size(grayscale.size().width / scale, grayscale.size().height / scale));
+
+		vector<Rect> faces;
+		faceCascade.detectMultiScale(grayscale, faces, 1.1, 3, 0, Size(30, 30));
+
+		for (Rect area : faces)
+		{
+			Scalar drawColor = Scalar(255, 255, 64);
+			
+			
+			rectangle(frame, Point(cvRound(area.x * scale), cvRound(area.y * scale)), Point(cvRound((area.x + area.width - 1) * scale),
+				cvRound((area.y + area.height - 1) * scale)), drawColor);
+
+
+		}
+		
+		imshow("webcam frame", frame);
+		
+
+		if (waitKey(30) >= 0)
+			break;
+	}
+	
+
+
+
 }
