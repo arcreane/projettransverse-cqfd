8476f9e105ca8abb36110f25efd2c4749be1c0ce
TIZEN77 <tizen.k18@gmail.com>
2021-03-16 09:51:16 +0100

    detection du visage

---
+++ detection.cpp
@@ -0,0 +1,58 @@
+#include <opencv2/opencv.hpp>
+#include <iostream>
+#include <iostream>
+#include <vector>
+
+
+using namespace std;
+using namespace cv;
+
+int main(int argc, const char** argv)
+{
+
+	double scale = 3.0;
+
+	CascadeClassifier faceCascade;
+	faceCascade.load("D:\\opencv\\build\\etc\\haarcascades\\haarcascade_frontalface_alt.xml");
+
+	VideoCapture cap(0);
+
+	if (!cap.isOpened())
+		return -1;
+
+	for (;;)
+	{
+		string path = "Resources//snapchat.png";
+		Mat img = imread(path);
+		Mat frame;
+		cap >> frame;
+
+		Mat grayscale;
+		cvtColor(frame, grayscale, COLOR_BGR2GRAY);
+		resize(grayscale, grayscale, Size(grayscale.size().width / scale, grayscale.size().height / scale));
+
+		vector<Rect> faces;
+		faceCascade.detectMultiScale(grayscale, faces, 1.1, 3, 0, Size(30, 30));
+
+		for (Rect area : faces)
+		{
+			Scalar drawColor = Scalar(255, 255, 64);
+
+
+			rectangle(frame, Point(cvRound(area.x * scale), cvRound(area.y * scale)), Point(cvRound((area.x + area.width - 1) * scale),
+				cvRound((area.y + area.height - 1) * scale)), drawColor);
+
+
+		}
+
+		imshow("webcam frame", frame);
+
+
+		if (waitKey(30) >= 0)
+			break;
+	}
+
+
+
+
+}
